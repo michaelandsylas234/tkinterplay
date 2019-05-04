687b02e84b522a3275d4824a3b337fd20c0801d6
Sylas-Hollander <50026745+Sylas-Hollander@users.noreply.github.com>
2019-05-04 18:34:31 -0400

    Commit

--- tkinterplay.py
+++ tkinterplay.py
@@ -1,9 +1,7 @@
-try:
-    # Python2
-    import Tkinter as tk
-except ImportError:
-    # Python3
-    import tkinter as tk
+
+    
+import tkinter as tk
+from tkinter import ttk
 def lift_win1():
     win1.lift(aboveThis=root)
 def lower_win1():
@@ -24,6 +22,6 @@ win1.title('top/child window win1')
 win1.geometry("300x150+120+120")
 btn_lift = tk.Button(win1, text="Why would you ever press this button?", command=lift_win1)
 btn_lift.pack(padx=30, pady=5)
-btn_lower = tk.Button(win1, text="MY MOM SAID SO", command=lower_win1)
+btn_lower = tk.Button(win1, text="MY MOM SAID SO", highlightbackground="blue", highlightthickness="30", command=lower_win1)
 btn_lower.pack(pady=5)
 win1.mainloop()
