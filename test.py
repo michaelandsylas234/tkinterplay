4d59a9e4449489ae8419a6bcf845efdc5bf67bc9
Sylas-Hollander <50026745+Sylas-Hollander@users.noreply.github.com>
2019-05-04 18:40:27 -0400

    coo mit

---
+++ test.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+# background="..." doesn't work...
+ttk.Style().configure('green/black.TLabel', foreground='green', background='black')
+ttk.Style().configure('green/black.TButton', foreground='green', background='black')
+
+label = ttk.Label(root, text='I am a ttk.Label with text!', style='green/black.TLabel') 
+label.pack()
+
+button = ttk.Button(root, text='Click Me!', style='green/black.TButton')
+button.pack()
