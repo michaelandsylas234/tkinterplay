6af04e28935cec9ffd9bcd1a17d227f1d0095da0
Sylas-Hollander <sylas2007@icloud.com>
2019-05-12 18:33:20 -0400

    Create nurses.py

---
+++ nurses.py
@@ -0,0 +1,24 @@
+import curses
+import time
+
+curses.initscr()
+
+def percentage():
+    loading = 0
+    while loading < 100:
+        loading += 1
+        time.sleep(0.03)
+        update_progress(loading)
+
+
+def update_progress(progress):
+    win = curses.newwin(3, 32, 3, 30)
+    win.border(0)
+    rangex = (30 / float(100)) * progress
+    pos = int(rangex)
+    display = '#'
+    if pos != 0:
+        win.addstr(1, 1, "{}".format(display*pos))
+        win.refresh()
+
+percentage()
