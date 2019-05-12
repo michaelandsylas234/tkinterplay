1b9236fde5582687593f9326ef5f337b5ad4ead6
Sylas-Hollander <sylas2007@icloud.com>
2019-05-12 18:32:28 -0400

    Potato potaato

--- purses.py
+++ purses.py
@@ -6,7 +6,7 @@ def main(stdscr):
 
     # This raises ZeroDivisionError when i == 10.
     for i in range(0, 11):
-        v = i-10
+        v = i-11
         stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
 
     stdscr.refresh()
